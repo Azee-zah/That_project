d88ca673a0980cdfc54b673b43bd4d8b50658bd4
Olamide Akinleye <olamideakinleye800@gmail.com>
2025-11-21 15:58:22 +0100

    still backend

--- Backend/main.py
+++ Backend/main.py
@@ -47,9 +47,41 @@ def sign_up(input: simple):
             "data": {"name": input.name, "email": input.email}
         }
 
+    except Exception as e:
+        raise HTTPException(status_code=501, detail=str(e))
+    
+class dimple(BaseModel):
+    email: str= Field('olamide@gmail.com')
+    password: str= Field('ola123')
+
+@app.post('/sign_in')
+def sign_in(input: dimple):
+    try:
+        #do we have you in the database?
+        query = text("""
+            SELECT * FROM users
+            WHERE email = :email
+        """)
+
+        ours = db.execute(query, {"email": input.email}).fetchone()
+        if not ours:
+            raise HTTPException(status_code=401, detail="Invalid email or password")
+        
+        our_password = bcrypt.checkpw(input.password.encode('utf-8'), ours.password.encode('utf-8'))
+        if not our_password:
+            raise HTTPException(status_code=401, detail="Invalid email or password")
+        
+        return{
+            "message": "User Logged in successfully",
+            "data": {input.email}
+        }
+        
+
 
     except Exception as e:
         raise HTTPException(status_code=501, detail=str(e))
+
+
     
 if __name__ == "__main__":
     uvicorn.run(app, host=os.getenv('host'), port=int(os.getenv('port')))
